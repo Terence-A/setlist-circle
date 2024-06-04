8edfbe3cef18d1dd5b88ccc916566002d2f66ad5
terenceDev <terence.arseneault@hotmail.com>
2024-06-04 08:05:15 -0300

    fix

---
+++ app/setlists/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Setlists = () => {
+  return (
+    <section className="flex flex-col items-center mt-20">
+      <h1 className="text-3xl text-amber-200 font-bold">Setlists</h1>
+      <p className="text-stone-50">Page in progress</p>
+    </section>
+  );
+};
+
+export default Setlists;
